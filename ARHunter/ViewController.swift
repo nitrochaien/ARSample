09a62a636df7143c396091d6afd29dfad14db493
namdv <vunam0502@gmail.com>
2018-10-01 14:44:06 +0700

    Initial Commit

---
+++ ARHunter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARHunter
+//
+//  Created by Nam Vu on 10/1/18.
+//  Copyright © 2018 Nam Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
