dba2521918eee2ba9eac3476f4a09c36aa26047a
namdv <vunam0502@gmail.com>
2018-10-07 16:26:28 +0700

    refactor code

--- ARHunter/ARView/Presenter/ARPresenter.swift
+++ ARHunter/ARView/Presenter/ARPresenter.swift
@@ -7,7 +7,81 @@
 //
 
 import UIKit
+import ARCL
+import CoreLocation
+import SceneKit
+
+protocol ARPresenterDelegate: NSObjectProtocol {
+    func didGenerateNodes(_ data: [LocationAnnotationNode])
+}
 
 class ARPresenter: NSObject {
+    weak var delegate: ARPresenterDelegate?
+    
+    var locations = [CLLocationCoordinate2D]()
+    var updateUserLocationTimer: Timer?
+    
+    func attachView(_ delegate: ARPresenterDelegate) {
+        self.delegate = delegate
+        
+        updateUserLocationTimer?.invalidate()
+        updateUserLocationTimer = Timer.scheduledTimer(
+            timeInterval: 0.5,
+            target: self,
+            selector: #selector(updateUserLocation),
+            userInfo: nil,
+            repeats: true)
+    }
+    
+    func detachView() {
+        delegate = nil
+        updateUserLocationTimer?.invalidate()
+        updateUserLocationTimer = nil
+    }
+    
+    @objc private func updateUserLocation() {
+        let data = generateDemoData()
+        delegate?.didGenerateNodes(data)
+    }
+    
+    func generateDemoData() -> [LocationAnnotationNode] {
+        var nodes = [LocationAnnotationNode]()
+        locations.forEach {
+            let lat = $0.latitude
+            let long = $0.longitude
+            let node = buildNode(latitude: lat, longitude: long, altitude: 10, imageName: "pin")
+            nodes.append(node)
+        }
+        return nodes
+    }
+    
+    private func buildNode(latitude: CLLocationDegrees, longitude: CLLocationDegrees, altitude: CLLocationDistance, imageName: String) -> LocationAnnotationNode {
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let location = CLLocation(coordinate: coordinate, altitude: altitude)
+        let image = UIImage(named: imageName)!
+        return LocationAnnotationNode(location: location, image: image)
+    }
+}
 
+// MARK: - SceneLocationViewDelegate
+extension ARPresenter: SceneLocationViewDelegate {
+    func sceneLocationViewDidAddSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
+        print("add scene location estimate, position: \(position), location: \(location.coordinate), accuracy: \(location.horizontalAccuracy), date: \(location.timestamp)")
+    }
+    
+    func sceneLocationViewDidRemoveSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
+        print("remove scene location estimate, position: \(position), location: \(location.coordinate), accuracy: \(location.horizontalAccuracy), date: \(location.timestamp)")
+    }
+    
+    func sceneLocationViewDidConfirmLocationOfNode(sceneLocationView: SceneLocationView, node: LocationNode) {
+        
+    }
+    
+    func sceneLocationViewDidSetupSceneNode(sceneLocationView: SceneLocationView, sceneNode: SCNNode) {
+        
+    }
+    
+    func sceneLocationViewDidUpdateLocationAndScaleOfLocationNode(sceneLocationView: SceneLocationView, locationNode: LocationNode) {
+        
+    }
 }
--- ARHunter/ARView/View/ARViewController.swift
+++ ARHunter/ARView/View/ARViewController.swift
@@ -9,31 +9,20 @@
 
 import UIKit
 import ARCL
-import SceneKit
 import CoreLocation
 
 class ARViewController: UIViewController {
-    let sceneView = SceneLocationView()
-
-    var locations = [CLLocationCoordinate2D]()
-    
-    var updateUserLocationTimer: Timer?
+    private let sceneView = SceneLocationView()
+    private let presenter = ARPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(sceneView)
         
-        sceneView.locationDelegate = self
-        
+        sceneView.locationDelegate = presenter
         addTapGestureToRemoveSceneView()
         
-        updateUserLocationTimer?.invalidate()
-        updateUserLocationTimer = Timer.scheduledTimer(
-            timeInterval: 0.5,
-            target: self,
-            selector: #selector(updateUserLocation),
-            userInfo: nil,
-            repeats: true)
+        presenter.attachView(self)
     }
     
     override func viewDidLayoutSubviews() {
@@ -42,9 +31,8 @@ class ARViewController: UIViewController {
         sceneView.frame = view.bounds
     }
 
-    
     func setData(locations: [CLLocationCoordinate2D]) {
-        self.locations = locations
+        presenter.locations = locations
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,30 +47,7 @@ class ARViewController: UIViewController {
         print("pause")
         // Pause the view's session
         sceneView.pause()
-        updateUserLocationTimer?.invalidate()
-        updateUserLocationTimer = nil
-    }
-    
-    @objc private func updateUserLocation() {
-        generateDemoData().forEach { sceneView.addLocationNodeWithConfirmedLocation(locationNode: $0) }
-    }
-    
-    private func generateDemoData() -> [LocationAnnotationNode] {
-        var nodes = [LocationAnnotationNode]()
-        locations.forEach {
-            let lat = $0.latitude
-            let long = $0.longitude
-            let node = buildNode(latitude: lat, longitude: long, altitude: 10, imageName: "pin")
-            nodes.append(node)
-        }
-        return nodes
-    }
-    
-    private func buildNode(latitude: CLLocationDegrees, longitude: CLLocationDegrees, altitude: CLLocationDistance, imageName: String) -> LocationAnnotationNode {
-        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let location = CLLocation(coordinate: coordinate, altitude: altitude)
-        let image = UIImage(named: imageName)!
-        return LocationAnnotationNode(location: location, image: image)
+        presenter.detachView()
     }
     
     private func addTapGestureToRemoveSceneView() {
@@ -91,33 +56,18 @@ class ARViewController: UIViewController {
     }
     
     @objc private func tapToSceneView(_ recognizer: UITapGestureRecognizer) {
-        let tapLocation = recognizer.location(in: sceneView)
-        let hitTestResults = sceneView.hitTest(tapLocation, options: nil)
-        
-        if let node = hitTestResults.first?.node {
-            
-        }
+//        let tapLocation = recognizer.location(in: sceneView)
+//        let hitTestResults = sceneView.hitTest(tapLocation, options: nil)
+//
+//        if let node = hitTestResults.first?.node {
+//
+//        }
     }
 }
 
-extension ARViewController: SceneLocationViewDelegate {
-    func sceneLocationViewDidAddSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
-        print("add scene location estimate, position: \(position), location: \(location.coordinate), accuracy: \(location.horizontalAccuracy), date: \(location.timestamp)")
-    }
-    
-    func sceneLocationViewDidRemoveSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
-        print("remove scene location estimate, position: \(position), location: \(location.coordinate), accuracy: \(location.horizontalAccuracy), date: \(location.timestamp)")
-    }
-    
-    func sceneLocationViewDidConfirmLocationOfNode(sceneLocationView: SceneLocationView, node: LocationNode) {
-        
-    }
-    
-    func sceneLocationViewDidSetupSceneNode(sceneLocationView: SceneLocationView, sceneNode: SCNNode) {
-        
-    }
-    
-    func sceneLocationViewDidUpdateLocationAndScaleOfLocationNode(sceneLocationView: SceneLocationView, locationNode: LocationNode) {
-        
+// MARK: - ARPresenterDelegate
+extension ARViewController: ARPresenterDelegate {
+    func didGenerateNodes(_ data: [LocationAnnotationNode]) {
+        data.forEach { sceneView.addLocationNodeWithConfirmedLocation(locationNode: $0) }
     }
 }
